6477437f87d94b4a3056ff5915215dcf147817ff
elijahp1595 <pina.elijah@gmail.com>
2026-04-26 21:02:47 +0000

    I have finished Mini01

--- week6-exercises/mini01-generic-box-and-methods/src/main/java/edu/norcocollege/cis18b/week6/mini01/GenericBoxDemo.java
+++ week6-exercises/mini01-generic-box-and-methods/src/main/java/edu/norcocollege/cis18b/week6/mini01/GenericBoxDemo.java
@@ -11,6 +11,13 @@ public class GenericBoxDemo {
         return values.get(0);
     }
 
+    public static <T> T last(List<T> values) {
+        if (values == null || values.isEmpty()) {
+            throw new IllegalArgumentException("values must contain at least one item");
+        }
+        return values.get(values.size() -1);
+    }
+
     public static void main(String[] args) {
         Box<String> labelBox = new Box<>();
         labelBox.set("CIS-18B");
@@ -18,12 +25,19 @@ public class GenericBoxDemo {
         Box<Integer> unitsBox = new Box<>();
         unitsBox.set(4);
 
-        String firstStudent = first(List.of("Ada", "Grace", "Margaret"));
-        Integer firstScore = first(List.of(98, 91, 87));
+        List<String> students = List.of("Ada", "Grace", "Margaret");
+        List<Integer> scores = List.of(98, 91, 87);
+
+        String firstStudent = first(students);
+        Integer firstScore = first(scores);
+
+        String lastStudent = last(students);
+        Integer lastScore = last(scores);
 
         System.out.println("Label: " + labelBox.get());
         System.out.println("Units: " + unitsBox.get());
         System.out.println("First student: " + firstStudent);
         System.out.println("First score: " + firstScore);
+
     }
 }
