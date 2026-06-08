7c133ce2d7b2aeec7e7ca58dfda34d2396e5b855
elijahp1595 <pina.elijah@gmail.com>
2026-06-08 22:51:10 +0000

    I have finished mini 4

--- week6-exercises/mini04-type-erasure-lab/src/main/java/edu/norcocollege/cis18b/week6/mini04/TypeErasureLab.java
+++ week6-exercises/mini04-type-erasure-lab/src/main/java/edu/norcocollege/cis18b/week6/mini04/TypeErasureLab.java
@@ -15,5 +15,17 @@ public class TypeErasureLab {
         System.out.println("Same runtime class: " + sameRuntimeClass);
         System.out.println("List is instance of java.util.List: " + listInstanceCheck);
         System.out.println("Course token simple name: " + TypeTokenPrinter.simpleNameOf(String.class));
+
+        /*
+        Type erasure removes generic type arguments at runtime.
+        At runtime, List<String> and List<Integer> are both just ArrayList.
+        Legal: courseNames instanceof List
+        Illegal: courseNames instanceof List<String>
+
+        API logging/diagnostics note:
+        Logs can show the runtime class, such as Arraylist,
+        but they usually cannot show wether it was List<String> or List<Integer>
+        unless the API receives extra type information, such as Class<T>.
+        */
     }
 }
